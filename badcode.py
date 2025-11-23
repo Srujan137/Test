e86f7139cb79995418b1c91c193f30eeff4afa3a
Thaduvai Srujan <thaduvaisrujan@MacBookAir.lan>
2025-11-22 19:15:35 -0500

    testing a good code

--- badcode.py
+++ badcode.py
@@ -1,2 +1,10 @@
-def broken_func()
-    print("this will never run"
+#def broken_func()
+#    print("this will never run"
+
+
+def add(a, b):
+    """Return sum of two numbers."""
+    return a + b
+
+if __name__ == "__main__":
+    print(add(2, 3))
