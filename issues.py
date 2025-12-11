68f6c14800dcd11170eb3e28a4bb412e7d9b8a00
Thaduvai Srujan <thaduvaisrujan@MacBookAir.lan>
2025-12-11 10:41:51 -0500

    Fix issues and clean up Test folder

--- issues.py
+++ issues.py
@@ -2,7 +2,6 @@ import requests
 import json
 
 SONAR_HOST = "http://localhost:9000"
-SONAR_TOKEN = "sqp_14af0a06617751172c30289ede45144b9494b576"
 PROJECT_KEY = "sample"
 
 url = f"{SONAR_HOST}/api/issues/search?projects={PROJECT_KEY}&resolved=false"
