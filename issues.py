d29331eebae3a9b40881ba993f35b2b42076916f
Thaduvai Srujan <thaduvaisrujan@MacBookAir.lan>
2025-12-11 10:57:49 -0500

    new check

--- issues.py
+++ issues.py
@@ -11,4 +11,4 @@ data = response.json()
 for issue in data.get("issues", []):
     print(f"File: {issue['component']}, Line: {issue['line']}, "
           f"Severity: {issue['severity']}, Rule: {issue['rule']}")
-    print(f"Message: {issue['message']}\n")
+    print(f"Messaadsge: {issue['message']}\n")
